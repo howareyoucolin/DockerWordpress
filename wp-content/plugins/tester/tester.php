f589b883fff07f2f7dff84f7dce871b89ce6d073
Your Name <you@example.com>
2019-07-06 02:58:15 +0000

    setup ACF pro

--- wp-content/plugins/tester/tester.php
+++ wp-content/plugins/tester/tester.php
@@ -8,10 +8,101 @@
 * Author URI: hhttps://www.369usa.com/
 **/
 
+if(true){
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+}
+
 //Security
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-function tester_func(){
-	echo 'The City of New York, usually called either New York City (NYC) or simply New York (NY), is the most populous city in the United States.[11] With an estimated 2018 population of 8,398,748[7] distributed over a land area of about 302.6 square miles (784 km2),[12][13] New York is also the most densely populated major city in the United States.[14] Located at the southern tip of the state of New York, the city is the center of the New York metropolitan area, the largest metropolitan area in the world by urban landmass.';
+//Adding an option page:
+function tester_create_option_page() {
+	if( function_exists('acf_add_options_page') ) {
+		$option_page = acf_add_options_page(array(
+			'page_title' 	=> 'Tester Options',
+			'menu_title' 	=> 'Tester Options',
+			'menu_slug' 	=> 'tester_options',
+			'capability' 	=> 'edit_posts',
+			'position' => 3,
+			'redirect' 	=> false
+		));
+	}
+	if( function_exists('acf_add_local_field_group') ):
+	acf_add_local_field_group(array(
+		'key' => 'group_5d1f771eaf87a',
+		'title' => 'Tester Group',
+		'fields' => array(
+			array(
+				'key' => 'field_5d1f771fd0bfe',
+				'label' => 'Header Text',
+				'name' => 'header_text',
+				'type' => 'text',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'maxlength' => '',
+			),
+			array(
+				'key' => 'field_5d1ff5a423c8a',
+				'label' => 'Tester Content',
+				'name' => 'tester_content',
+				'type' => 'textarea',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'maxlength' => '',
+				'rows' => 16,
+				'new_lines' => '',
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'options_page',
+					'operator' => '==',
+					'value' => 'tester_options',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+	));
+	endif;
 }
-add_action('tester_plugin_hook','tester_func');
+
+//Adding text to frontend:
+function tester_func() {
+	update_field('header_text', 'I Love Flushing', 'option');//Use ACF update function
+	$header = get_field('header_text', 'option');
+	$content = get_field('tester_content', 'option');
+	echo $header . ' --- ' . $content;
+}
+
+//Add actions to hook:
+add_action('admin_init', 'tester_create_option_page',10,0);
+add_action('admin_menu', 'tester_create_option_page',10,0);
+add_action('tester_plugin_hook','tester_func',10,0);
